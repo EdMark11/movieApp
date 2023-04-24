fbd17c3fc4639ed4a913f3f04b34ba5cece2e99f
edgarasmarkunas <markunas11@gmail.com>
2023-04-24 17:18:52 +0100

    design changes for card backgroud and title

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -4,8 +4,10 @@ import MovieCard from "./MovieCard"
 const Popular = ({movies}) => {
   return (
     <div className="bg-gray-700 container max-w-7xl mx-auto pb-10 px-4">
-        <h1 className="text-white text-2xl mt-8 mb-5">Popular</h1>
+      <div className="bg-slate-600 px-10 rounded-md pb-8 ">
+        <h1 className="text-white text-2xl pt-4 mt-10 mb-5 font-semibold">Popular</h1>
         <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">{movies.map(movie => <MovieCard movie={movie} key={movie.id}/>)}</div>
+      </div>
     </div>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,8 +4,8 @@ import Meta from "@/components/Meta"
 const contact = () => {
   return (
     <div className="text-center mt-20">
-      <Meta title="Contact Us"/>
-      <h1 className="text-4xl font-bold">contact us</h1>
+      <Meta title="Random Movie"/>
+      <h1 className="text-4xl font-bold">Random Movie</h1>
     </div>
   )
 }
