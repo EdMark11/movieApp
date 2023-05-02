5ba5aca0527facb9cb18ca4a34d5defe7295add0
edgarasmarkunas <markunas11@gmail.com>
2023-05-02 17:24:00 +0100

    working on removing white area

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,7 +5,7 @@ const Landing = () => {
   return (
         <div className='text-center container bg-white max-w-xl mx-auto pb-10 rounded-xl'>
             <div className='w-60 mx-auto'>
-                <Image src={"/watchmovie.png"} height={250} width={250} alt=""/>
+                <Image className="" src={"/watchmovie.png"} height={250} width={250} alt=""/>
             </div>
             <h1 className="text-2xl text-gray-700 uppercase font-bold">Welcome to What's Poppin'</h1>
             <p className="text-gray-500">Browse the most popular movies right now!</p>
